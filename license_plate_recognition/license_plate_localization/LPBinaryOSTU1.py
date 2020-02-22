5b7a68ca6331855aa9bbfb8b5c009eaf50ebbafe
pentiumCM <842679178@qq.com>
2020-02-22 20:39:51 +0800

    整理项目目录，补充训练模型，数据集等文件

--- license_plate_recognition/license_plate_localization/LPBinaryOSTU1.py
+++ license_plate_recognition/license_plate_localization/LPBinaryOSTU1.py
@@ -6,22 +6,32 @@ time:2020-1-27
 import cv2
 import numpy as np
 
+import joblib as jl
+
 # 定义常量
-minArea = 2000  #车牌区域允许最大面积
+minArea = 2000  # 车牌区域允许最大面积
+
+# 加载分类器model
+clf = jl.load('../../docs/model/nb_clf_6.pkl')
+# 加载数据标准化的模型
+scaler = jl.load('../../docs/scaler/scaler.pkl')
 
 '''获取图片的像素点，再重新计算像素点输出图片'''
 '''论文A Novel Least Squre and Image Rotation based Method for ...第5995页的描述实现'''
+
+
 def getPiex(img):
-    row = img.shape[0]    # 图片的行
-    col = img.shape[1]    # 图片的列
+    row = img.shape[0]  # 图片的行
+    col = img.shape[1]  # 图片的列
     for i in range(row - 1):
         for j in range(col - 1):
-            if (int(img[i+1, j]) - int(img[i, j]) >= 50):
+            if (int(img[i + 1, j]) - int(img[i, j]) >= 50):
                 img[i, j] = 255
             else:
                 img[i, j] = 0
 
     return img
+
 
 # 灰度拉伸函数
 def stretch(img):
@@ -30,9 +40,10 @@ def stretch(img):
 
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            img[i, j] = (255/(max-min))*img[i, j]-(255*min)/(max-min)
+            img[i, j] = (255 / (max - min)) * img[i, j] - (255 * min) / (max - min)
 
     return img
+
 
 def lpr(filename):
     # img = cv2.imread(filename)
@@ -47,21 +58,21 @@ def lpr(filename):
     # RGB转灰色
     grayImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    # 灰度拉伸(灰的更灰，白的更白）
+    stretchedImg = stretch(grayImg)
+
     # 直方图均衡Histogram Equalization。全局图像质量的提升，可能会丢失一些局部图像的细节。比如，脸被画出来了，
     # 但是脸上的鼻子的轮廓可能就没有了，所以有时需要采用自适应直方图均衡
-    HEImg = cv2.equalizeHist(grayImg)
+    HEImg = cv2.equalizeHist(stretchedImg)
     # cv2.imshow("HEImg", HEImg)
     # 自适应直方图均衡。局部图像均衡化
     # HEImgOrg = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     # HEImg = HEImgOrg.apply(grayImg)
     # cv2.imshow("HEImg", HEImg)
 
-    # 灰度拉伸(灰的更灰，白的更白）
-    stretchedImg = stretch(HEImg)
-
     # 输出的ndarray列表的维度483行300列 print(grayImg.shape)
     # 高斯滤波平滑处理，第4个参数设为0，表示不计算y方向的梯度，原因是车牌上的数字在竖方向较长，重点在于得到竖方向的边界
-    GaussianBlurImg = cv2.GaussianBlur(stretchedImg, (3, 3), 0)
+    GaussianBlurImg = cv2.GaussianBlur(HEImg, (3, 3), 0)
     # 采用Sobel进行边缘提取
     SobelImg = cv2.Sobel(GaussianBlurImg, -1, 1, 0, ksize=3)
     # 图像二值化。采用像素间的计算
@@ -114,16 +125,37 @@ def lpr(filename):
         whRatio = areaWidth / areaHeight  # 计算长宽比
         # 要求矩形区域长宽比在2到5.5之间，2到5.5是车牌的长宽比，其余的矩形排除
         if whRatio > 2 and whRatio < 5.5:
+
             carContours.append(rect)
             box = cv2.boxPoints(rect)
-            print(rect)
-            print(cnt)
-            print(cnt[1][0])
+            print("rect：", rect)
+            # print("cnt：",cnt)
+            # print("cnt[1][0]：",cnt[1][0])
 
-            print('中国')
-            print(box)
+            # print('中国')
+            # print("box：",box)
             box = np.int0(box)
-            cv2.drawContours(image, [box], 0, (0, 255, 255), 2)
+
+            # 对新的样本进行预测
+            area = areaWidth * areaHeight  # 求区域的面积
+            x, y = rect[0]
+            angle = rect[2]
+            sample = np.array([x, y, areaWidth, areaHeight, angle, area])
+            sample = np.reshape(sample, (1, -1))
+
+            # 引用数据标准化模型
+            global scaler
+            sample_scaler = scaler.transform(sample)
+
+            # 引用模型分类器
+            global clf
+            label = clf.predict(sample_scaler)
+            print("类别标签：", label)
+            # 如果预测的标签为0，则退出当前，执行下一循环
+            if (label == 1):
+                cv2.drawContours(image, [box], 0, (255, 0, 0), 2)
+            else:
+                cv2.drawContours(image, [box], 0, (0, 255, 255), 2)
 
     # 将轮廓规整为长方形
     rectangles = []
@@ -160,10 +192,10 @@ def lpr(filename):
             maxMean = mean[0]
             distR = r
 
-    cv2.rectangle(image, (distR[0] + 3, distR[1]), (distR[2] - 3, distR[3]), (0, 255, 0), 2)
+    # cv2.rectangle(image, (distR[0] + 3, distR[1]), (distR[2] - 3, distR[3]), (0, 255, 0), 2)
     # cv2.rectangle(image, (distR[0], distR[1]), (distR[2], distR[3]), (0, 255, 0), 2)
 
-    cv2.imshow("image"+ str(i + 1) + ".jpg", image)
+    cv2.imshow("image" + str(i + 1) + ".jpg", image)
     # cv2.imshow("grayImg", grayImg)
     # cv2.imshow("stretchedImg", stretchedImg)
     # cv2.imshow("openingImg", openingImg)
@@ -177,6 +209,7 @@ def lpr(filename):
 
 
 if __name__ == "__main__":
+
     # 循环读取文件
     for i in range(100):
         # lpr("../../Img/common/" + str(i + 1) + ".jpg")
---
+++ license_plate_recognition/license_plate_localization/svm_clf.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@Author  : pentiumCM
+@Email   : 842679178@qq.com
+@Software: PyCharm
+@File    : svm_clf.py
+@Time    : 2020/2/19 22:12
+@desc	 : 车牌定位的SVM分类器
+'''
+
+import numpy as np
+import pandas as pd
+import joblib as jl
+import operator
+from functools import reduce
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+
+
+def make_data_set(data_file, dimens, scaler_path):
+    """
+    读取数据集中的数据，制作数据集
+
+    :param data_file: 数据集路径
+    :param dimens: 训练集的维度
+    :param scaler_path: 数据标准化模型的路径
+    :return: 返回训练集，data,labels
+    """
+    # 读取数据集CSV文件，并且过滤掉表头
+    # dataset = pd.read_csv(data_file, skiprows=1)
+    dataset = pd.read_csv(data_file)
+    # 获取样本数据集中的特征数据
+    # features = ['横坐标X', '纵坐标Y', '宽度', '高度', '面积']
+    data = dataset.iloc[0:, 0:dimens]  # 从第一行到最后一行，第1列到第dimens列
+    # 获取样本的类别标签labels
+    labels = dataset.iloc[0:, dimens:]  # 从第一行到最后一行，第dimens+1列
+
+    # 调整数据集数组的维数
+    data = np.reshape(np.array(data), (-1, dimens))
+    labels = np.reshape(np.array(labels), (1, -1))
+
+    # 数据标准化
+    scaler = preprocessing.StandardScaler().fit(data)  # 用训练集数据训练scaler
+    X_scaled = scaler.transform(data)  # 用其转换训练集是理所当然的
+
+    labels = reduce(operator.add, labels)  # 将标签二维数组转化为一维数组
+
+    # 保存数据标准化的模型
+    jl.dump(scaler, scaler_path)
+    print("数据标准化模型保存成功")
+    return X_scaled, labels
+
+
+def svm_classifier(train_data, train_labels, model_file):
+    """
+    采用SVM分类器，进行分类
+
+    :param train_data: 训练集数据
+    :param train_labels: 训练集标签
+    :param model_file: 保存的模型文件路径
+    :return: 模型文件保存到本地
+    """
+
+    # X = [[0, 0], [1, 1]]
+    # y = [0, 1]
+
+    # 主要调节的参数有：C、kernel、degree、gamma、coef0。
+    clf = svm.SVC(C=0.4, cache_size=200, class_weight=None, coef0=0.0,
+                  decision_function_shape='ovr', degree=3, gamma='scale', kernel='rbf',
+                  max_iter=-1, probability=False, random_state=None, shrinking=True,
+                  tol=0.001, verbose=False)
+    clf.fit(train_data, train_labels)
+
+    # save model
+    jl.dump(clf, model_file)
+
+    print("模型文件保存成功")
+
+
+if __name__ == '__main__':
+    # 定义采用数据集的路径
+    data_path = '../../docs/dataset/write_data_7.csv'
+
+    # 定义训练集数据的维度
+    dimen = 6
+
+    # 定义数据标准化模型的路径
+    scaler_path = '../../docs/scaler/scaler.pkl'
+
+    # 定义生成模型的保存路径
+    model_path = '../../docs/model/svm_clf_6.pkl'
+
+    # 构造数据集
+    data, labels = make_data_set(data_path, dimen, scaler_path)
+
+    # 划分数据集为：训练集和测试集
+    data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.4, random_state=0)
+
+    # 构建分类器
+    svm_classifier(data_train, labels_train, model_path)
+
+    # load model
+    clf2 = jl.load(model_path)
+    # y_pred = clf2.predict(data)
+    score = clf2.score(data_test, labels_test)
+    print("准确率：", score)
+
+    # print("SVM分类器，样本总数： %d 错误样本数 : %d" % (data.shape[0], (labels != y_pred).sum()))
